ef1e858cea1a973f1f77fac19d2cac36f46f974a
хмдн++ <smcartix@gmail.com>
2025-09-26 16:24:16 +0000

    update submodules manually (again)

--- Telegram/build/prepare/linux.sh
+++ Telegram/build/prepare/linux.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 set -e
+
+export DOCKER_BUILDKIT=1
+
+TOTAL_MEM=$(free -b | awk '/^Mem:/{print $2}')
+SAFE_MEM=$((TOTAL_MEM - 4*1024*1024*1024))
+MAX_MEM=$((16*1024*1024*1024))
+
+DOCKER_MEM=$((SAFE_MEM < MAX_MEM ? SAFE_MEM : MAX_MEM))
+
 FullExecPath=$PWD
 pushd `dirname $0` > /dev/null
 FullScriptPath=`pwd`
 popd > /dev/null
 
-
-cd $FullScriptPath/../docker/centos_env
+cd "$FullScriptPath/../docker/centos_env"
 poetry install
-poetry run gen_dockerfile | DOCKER_BUILDKIT=1 docker build -t tdesktop:centos_env -
-cd $FullExecPath
+poetry run gen_dockerfile | docker build \
+    --memory=$DOCKER_MEM \
+    --memory-swap=$DOCKER_MEM \
+    -t tdesktop:centos_env -
+
+cd "$FullExecPath"
