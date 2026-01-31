cce08ae47bc93919c8c49b71274cf9e48b0c9bdc
xmdnx <smcartix@gmail.com>
2026-01-31 14:48:09 +0300

    fix: scrolling emoji packs in main menu with touchpad

--- Telegram/SourceFiles/window/window_main_menu.cpp
+++ Telegram/SourceFiles/window/window_main_menu.cpp
@@ -1025,6 +1025,9 @@ void MainMenu::setupSwipe() {
 		if (direction != Qt::LeftToRight) {
 			return Ui::Controls::SwipeHandlerFinishData();
 		}
+		if (_emojiStatusPanel && _emojiStatusPanel->hasFocus()) {
+			return Ui::Controls::SwipeHandlerFinishData();
+		}
 		return Ui::Controls::DefaultSwipeBackHandlerFinishData([=] {
 			closeLayer();
 		});
